ff2cb2a5b5fc192a2ec1aae162d677e175e13a56
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2021-07-19 20:06:30 +1000

    filter logic added

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { EventService } from "../shared/event.service"
 import { ActivatedRoute } from "@angular/router"
 import { IEvent, ISession } from "../shared"
@@ -12,9 +12,10 @@ import { IEvent, ISession } from "../shared"
     `]
 })
 
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit {
     event:IEvent
     addMode:Boolean
+    filterBy: string = 'all';
     
     constructor(private eventService:EventService, private route:ActivatedRoute) {
 
--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from "../shared/index";
 
 @Component({
@@ -7,5 +7,23 @@ import { ISession } from "../shared/index";
 })
 
 export class SessionListComponent {
-    @Input() sessions:ISession
+    @Input() sessions:ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
+
+    ngOnChanges() {
+      if(this.sessions) {
+          this.filterSessions(this.filterBy);
+      }
+    }
+
+    filterSessions(filter) {
+        if(filter === 'all') {
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+        }
+    }
 }
